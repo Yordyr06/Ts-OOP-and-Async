90b600dd08834edd82821a8cf631357c00501511
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-21 09:40:08 +0000

    Advanced: Protected

---
+++ src/classes/09-protected.ts
@@ -0,0 +1,32 @@
+export class Animal {
+  constructor(protected name: string) {};
+
+  move() {
+    console.log("Moving along!");
+  }
+
+  greeting() {
+    return `Hello I'm ${this.name}`
+  }
+}
+
+export class Dog extends Animal {
+  constructor(
+    name: string,
+    public owner: string
+  ) {
+    super(name);
+  }
+
+  woof(times: number) {
+    for (let index = 0; index < times; index++) {
+      console.log("woof");
+    }
+  }
+};
+
+const cyndie = new Dog('Cyndie', 'Yordy');
+cyndie.move();
+console.log(cyndie.greeting());
+// cyndie.name
+cyndie.owner
