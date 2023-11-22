84406989f83824721801b69d9ac19ff59798131a
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-22 02:33:34 -0400

    Advanced: Abstract Classes

---
+++ src/classes/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal, Dog } from "./09-protected";
+
+// const animal = new Animal('Alan');
+// animal.greeting();
+
+const rocky = new Dog('Rocky', 'Karla');
+rocky.greeting();
+console.log(rocky.owner);
